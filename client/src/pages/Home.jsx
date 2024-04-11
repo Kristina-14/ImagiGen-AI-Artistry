df6acd4c0a3439214e598013459e47d837428cd1
Kristina140699 <Kristina.11915169@lpu.in>
2024-04-11 12:20:22 +0530

    representation of home page done!

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { Card, FormField, Loader } from '../components'
 
+const RenderCards = ({ data, title }) => {
+  if(data?.length > 0){
+    return data.map((post) => <Card key={post._id} {...post}/>)
+  }
+return (
+  <h2 className='mt-5 font-bold text-[#0c4a6e] text-xl uppercase'>{title}</h2>
+)
+}
+
 const Home = () => {
   const [loading, setLodading] = useState(false); // creating a loading state
   const [allPosts, setAllPosts] = useState(null); // creating a state to store all posts, initialized with NULL
@@ -30,13 +39,26 @@ const Home = () => {
   <Loader /> 
   </div>
   :
-  <div>
+  <>
     {searchText && (
       <h2 className='font-medium text-[#475569] text-xl mb-3'>
-        Showing results for: <span className='text-[#020617]'>{searchText}</span>
+        Showing results for: <span className='text-[#020617]'>
+          {searchText}</span>
       </h2>
     )}
-  </div>}
+    {/* Bcoz I want to show a gri of images in the gallery so used className='grid' */}
+    <div className='grid lg:grid-cols-4 sm:grid-cols-3
+    xs:grid-cols-2 grid-cols-1 gap-3'>
+      {searchText ? (<RenderCards
+        data={[]} //"searchResults"
+        title="No search results found!!"
+      />)
+      :
+      <RenderCards 
+      data={[]}  //"allPosts"
+      title = "No posts found"/>}
+    </div>
+  </>}
 
 </div>
 
