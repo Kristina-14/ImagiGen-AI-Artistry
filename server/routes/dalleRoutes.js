6fbeb2fff8ca7ce7e9f396e8aabfde92d128d22f
Kristina-14 <kristina.barooah@lpu.in>
2024-04-14 10:03:41 +0530

    Open AI api working. Dall E api calls setup!

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import OpenAI from 'openai';
+import axios from 'axios';
+import fs from 'fs';
 
 dotenv.config();
 
 const router = express.Router();
 
 const configuration = {
-    // apiKey: 'OPENAI_API_KEY = "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"'
     apiKey: process.env.OPENAI_API_KEY
 };
 
-const openai = new OpenAI(configuration);
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 router.route('/').get((req, res) => {
     res.send('Great the API is working!!');
@@ -19,17 +20,20 @@ router.route('/').get((req, res) => {
 
 router.route('/').post(async (req, res) => {
     try {
-        const { prompt } = req.body;
+        const prompt = req.body.prompt;
+        console.log(prompt)
 
-        const aiResponse = await openai.image.create({
+        const aiResponse = await openai.images.generate({
             prompt,
             n: 1,
             size: '1024x1024',
-            responseType: 'b64_json',
         });
 
-        const image = aiResponse.data[0];
-         res.status(200).json({ photo: image });
+        console.log(aiResponse.data[0].url)
+        const response = await axios.get(aiResponse.data[0].url, { responseType: 'arraybuffer' });
+        const imageData = Buffer.from(response.data, 'binary').toString('base64');
+
+         res.status(200).json({ photo: imageData });
     } catch (error) {
         console.log(error);
         res.status(500).send(error?.response?.data?.error?.message);
